6c3d63342b79024877e1b29b1582836d0d18b0e7
iiriz123 <iiris.sydanmaki@gmail.com>
2026-02-06 21:43:00 +0200

    Add classes to items

--- app.py
+++ app.py
@@ -50,7 +50,8 @@ def show_item(item_id):
     item = items.get_item(item_id)
     if not item:
         abort(404)
-    return render_template("show_item.html", item=item)
+    classes = items.get_classes(item_id)
+    return render_template("show_item.html", item=item, classes=classes)
 
 @app.route("/new_item")
 def new_item():
@@ -71,7 +72,15 @@ def create_item():
         abort(403)
     user_id = session["user_id"]
 
-    items.add_item(destination, start_date, end_date, description, user_id)
+    classes = []
+    trip_type = request.form["trip_type"]
+    if trip_type:
+        classes.append(("Matkan luonne", trip_type))
+    budget = request.form["budget"]
+    if trip_type:
+        classes.append(("Budjetti", budget))
+
+    items.add_item(destination, start_date, end_date, description, user_id, classes)
     return redirect("/")
 
 @app.route("/edit_item/<int:item_id>")
